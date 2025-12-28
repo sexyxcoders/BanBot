1f76b88a15b15d025f0b7e466bc81af4274ceea4
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-28 21:37:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
 
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+MONGO_URI = os.getenv("MONGO_URI")
+ADMIN_GROUP_ID = int(os.getenv("ADMIN_GROUP_ID"))
+
+REFERRAL_AMOUNT = 5
+MIN_REFERRALS = 5
