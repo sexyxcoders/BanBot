49ca2eb722966658facd74064571c47b3d95c03f
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-28 21:36:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
+from pyrogram import Client
+import config
 
+app = Client(
+    "ReferralBot",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN
+)
+
+# Load handlers
+import handlers.start
+import handlers.menu
+import handlers.profile
+import handlers.refer
+import handlers.reward
+import handlers.request
+import handlers.feedback
+import handlers.admin
+
+app.run()
