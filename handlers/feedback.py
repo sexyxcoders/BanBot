f497305fe7ee4f971ecee4ce5765fb74fb3ca96d
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-30 17:15:24 +0530

    Update feedback.py

--- handlers/feedback.py
+++ handlers/feedback.py
@@ -1,1 +1,6 @@
+from pyrogram import filters
+from bot import app
 
+@app.on_callback_query(filters.regex("feedback"))
+async def feedback(_, q):
+    await q.message.reply("📝 Please send your feedback message.")
