e3296dde54caa6b7d13b861bf78bb030f0611fee
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-30 17:21:32 +0530

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -1,1 +1,6 @@
+from database.users import get_user
+from utils.constants import MIN_REFERRALS
 
+def has_min_referrals(user_id: int) -> bool:
+    user = get_user(user_id)
+    return user["referrals"] >= MIN_REFERRALS
