3535df363429854844c3d567494c2f458d9db51e
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-30 17:17:16 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,1 +1,8 @@
+from pymongo import MongoClient
+from config import MONGO_URI, DB_NAME
 
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+
+users_col = db["users"]
+requests_col = db["requests"]
