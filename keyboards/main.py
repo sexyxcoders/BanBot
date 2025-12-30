0296ad34db7dd9edc762722fc50e0c62e8534ad1
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-30 17:18:50 +0530

    Update main.py

--- keyboards/main.py
+++ keyboards/main.py
@@ -1,1 +1,14 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+main_inline = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("📝 Feedback", callback_data="feedback"),
+            InlineKeyboardButton("🎁 Reward", callback_data="reward")
+        ],
+        [
+            InlineKeyboardButton("👤 Profile", callback_data="profile"),
+            InlineKeyboardButton("👥 Refer", callback_data="refer")
+        ]
+    ]
+)
