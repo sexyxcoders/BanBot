37688a0961c1c9616176ef3ab90ab2cd020cb197
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-30 17:14:18 +0530

    Update refer.py

--- handlers/refer.py
+++ handlers/refer.py
@@ -1,1 +1,10 @@
+from pyrogram import filters
+from bot import app
 
+@app.on_callback_query(filters.regex("refer"))
+async def refer(_, q):
+    link = f"https://t.me/{app.username}?start={q.from_user.id}"
+    await q.message.reply(
+        f"👥 **Your Referral Link**\n\n{link}\n\n"
+        "💸 Earn ₹5 per referral"
+    )
