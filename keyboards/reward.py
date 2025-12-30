b90ad126322597fdba66852ff9abde6be8510bee
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-30 17:19:06 +0530

    Update reward.py

--- keyboards/reward.py
+++ keyboards/reward.py
@@ -1,1 +1,18 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+reward_keyboard = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("⭐ Telegram Premium", callback_data="reward_premium")
+        ],
+        [
+            InlineKeyboardButton("🌟 Stars", callback_data="reward_stars")
+        ],
+        [
+            InlineKeyboardButton("🎁 Profile / Gifts", callback_data="reward_gift")
+        ],
+        [
+            InlineKeyboardButton("🔙 Back", callback_data="menu")
+        ]
+    ]
+)
