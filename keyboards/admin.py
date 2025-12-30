8fb879dd2f0f2c0eac2d19cf63e9f426f03516f7
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-30 17:19:24 +0530

    Update admin.py

--- keyboards/admin.py
+++ keyboards/admin.py
@@ -1,1 +1,17 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def admin_request_keyboard(user_id: int):
+    return InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "✅ Approve",
+                    callback_data=f"approve_{user_id}"
+                ),
+                InlineKeyboardButton(
+                    "❌ Reject",
+                    callback_data=f"reject_{user_id}"
+                )
+            ]
+        ]
+    )
