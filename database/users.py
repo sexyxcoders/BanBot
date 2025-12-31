6b1430ca9b783b7efccd99e3806bb4b5c5620f5e
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-31 22:54:42 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -3,16 +3,25 @@ from database.mongo import get_db
 def _col():
     return get_db()["users"]
 
+def add_user(user_id: int):
+    """Create user if not exists"""
+    _col().update_one(
+        {"_id": user_id},
+        {
+            "$setOnInsert": {
+                "referrals": 0,
+                "balance": 0,
+                "joined": False
+            }
+        },
+        upsert=True
+    )
+
 def get_user(user_id: int):
     user = _col().find_one({"_id": user_id})
     if not user:
-        user = {
-            "_id": user_id,
-            "referrals": 0,
-            "balance": 0,
-            "joined": False
-        }
-        _col().insert_one(user)
+        add_user(user_id)
+        user = _col().find_one({"_id": user_id})
     return user
 
 def add_referral(user_id: int):
