2d625511aebed27b0e262c74241ba5fd136032ef
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-31 23:08:47 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,2 @@
-from .start import *
-from .menu import *
-from .profile import *
-from .refer import *
-from .reward import *
-from .request import *
-from .feedback import *
-from .admin import *
+# DO NOT IMPORT ANYTHING HERE
+# Handlers are loaded explicitly in bot.py
