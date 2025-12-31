89cb8245e93d70bdb059f9e53797714c4c7ddbca
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-31 22:36:52 +0530

    Update refer.py

--- handlers/refer.py
+++ handlers/refer.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from bot import app
 
-@app.on_callback_query(filters.regex("refer"))
-async def refer(_, q):
+@app.on_callback_query(filters.regex("^refer$"))
+async def refer_handler(_, q):
     link = f"https://t.me/{app.username}?start={q.from_user.id}"
     await q.message.reply(
         f"👥 **Your Referral Link**\n\n{link}\n\n"
