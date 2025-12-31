52a711440271291e8a1c4c246b45ae92c0ff5f30
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-31 22:34:41 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -1,25 +1,36 @@
-from database.mongo import users_col
+from database.mongo import users
 from datetime import datetime
 
 def add_user(user_id: int):
-    if not users_col.find_one({"_id": user_id}):
-        users_col.insert_one({
-            "_id": user_id,
-            "referrals": 0,
-            "referred_users": [],
-            "balance": 0,
-            "joined": datetime.utcnow()
-        })
-
-def add_referral(referrer_id: int, new_user_id: int):
-    user = users_col.find_one({"_id": referrer_id})
-    if not user:
+    if users.find_one({"_id": user_id}):
         return
 
-    if new_user_id in user.get("referred_users", []):
-        return  # prevent duplicate referral
+    users.insert_one({
+        "_id": user_id,
+        "referrals": 0,
+        "referred_users": [],
+        "balance": 0,
+        "joined_at": datetime.utcnow()
+    })
 
-    users_col.update_one(
+
+def get_user(user_id: int):
+    user = users.find_one({"_id": user_id})
+    if not user:
+        add_user(user_id)
+        user = users.find_one({"_id": user_id})
+    return user
+
+
+def add_referral(referrer_id: int, new_user_id: int):
+    referrer = users.find_one({"_id": referrer_id})
+    if not referrer:
+        return False
+
+    if new_user_id in referrer.get("referred_users", []):
+        return False  # duplicate referral
+
+    users.update_one(
         {"_id": referrer_id},
         {
             "$inc": {
@@ -31,16 +42,4 @@ def add_referral(referrer_id: int, new_user_id: int):
             }
         }
     )
-
-def get_user(user_id: int):
-    user = users_col.find_one({"_id": user_id})
-    if not user:
-        add_user(user_id)
-        user = users_col.find_one({"_id": user_id})
-    return user
-
-def reset_referrals(user_id: int):
-    users_col.update_one(
-        {"_id": user_id},
-        {"$set": {"referrals": 0, "balance": 0}}
-    )
+    return True
