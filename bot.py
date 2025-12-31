9842e3df23b5eba6019bce6300852d2b5e7e8f4c
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-31 23:17:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 
 API_ID = int("22657083")
 API_HASH = "d6186691704bd901bdab275ceaab88f3"
-BOT_TOKEN = "8561663678:AAHrubzNYuGYnrtyMMQzWBPgAEagKKsrGIQ"
+BOT_TOKEN = "8527251945:AAE8Uc7VT_yQbCo0IL_uWWIChdn4nQWPizU"
 
 app = Client("testbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
