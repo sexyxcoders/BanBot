7cbd8476d200ccc23eaf2471c9389009a5bde76f
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-31 22:48:57 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -1,45 +1,30 @@
-from database.mongo import users
-from datetime import datetime
+from database.mongo import get_db
 
-def add_user(user_id: int):
-    if users.find_one({"_id": user_id}):
-        return
-
-    users.insert_one({
-        "_id": user_id,
-        "referrals": 0,
-        "referred_users": [],
-        "balance": 0,
-        "joined_at": datetime.utcnow()
-    })
-
+def _col():
+    return get_db()["users"]
 
 def get_user(user_id: int):
-    user = users.find_one({"_id": user_id})
+    user = _col().find_one({"_id": user_id})
     if not user:
-        add_user(user_id)
-        user = users.find_one({"_id": user_id})
+        user = {
+            "_id": user_id,
+            "referrals": 0,
+            "balance": 0,
+            "joined": False
+        }
+        _col().insert_one(user)
     return user
 
-
-def add_referral(referrer_id: int, new_user_id: int):
-    referrer = users.find_one({"_id": referrer_id})
-    if not referrer:
-        return False
-
-    if new_user_id in referrer.get("referred_users", []):
-        return False  # duplicate referral
-
-    users.update_one(
-        {"_id": referrer_id},
-        {
-            "$inc": {
-                "referrals": 1,
-                "balance": 5
-            },
-            "$push": {
-                "referred_users": new_user_id
-            }
-        }
+def add_referral(user_id: int):
+    _col().update_one(
+        {"_id": user_id},
+        {"$inc": {"referrals": 1, "balance": 5}},
+        upsert=True
     )
-    return True
+
+def mark_joined(user_id: int):
+    _col().update_one(
+        {"_id": user_id},
+        {"$set": {"joined": True}},
+        upsert=True
+    )
